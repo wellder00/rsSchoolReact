59d5c4bd9ffbf22fda62af1e3a6c0478b989a785
wellder00 <wellder00@gmail.com>
2023-10-18 23:21:55 +0300

    init: create new react rs school project

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { Component, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Will make a new render</div>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
